4bfc629fd14210822abd13369ff3680ee7742820
makalagovardhan30799 <mgovardhangoud311@gmail.com>
2024-06-23 20:16:47 +0530

    practicing variable concept in shell scripting

---
+++ 02-variable.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Govardhan:: Hi Chenchu, Good Morning!"
+echo "Chenchu:: Hi Gova, Very Good morining"
+echo "Govardhan:: How are you chenchu?"
+echo "chenchu:: Iam fine gova how about you?"
+echo "Govardhan:: Iam also good chenchu"
