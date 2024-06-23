0f0c4c185dd1f2ffa6359219ecf2b1673e52122a
makalagovardhan30799 <mgovardhangoud311@gmail.com>
2024-06-23 20:28:15 +0530

    Some modificatins in variables file

--- 02-variable.sh
+++ 02-variable.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-echo "Govardhan:: Hi Chenchu, Good Morning!"
-echo "Chenchu:: Hi Gova, Very Good morining"
-echo "Govardhan:: How are you chenchu?"
-echo "chenchu:: Iam fine gova how about you?"
-echo "Govardhan:: Iam also good chenchu"
+PERSON1=Govardhan
+PERSON2=Chenchu
+
+echo "$PERSON1:: Hi $PERSON2, Good Morning!"
+echo "$PERSON2:: Hi $PERSON1, Very Good morining"
+echo "$PERSON1:: How are you $PERSON2?"
+echo "$PERSON2:: Iam fine $PERSON1 how about you?"
+echo "$PERSON1:: Iam also good $PERSON2"
