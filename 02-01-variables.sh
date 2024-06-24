0c395d71bf0fad1883eb7abb596b8e28d7448156
makalagovardhan30799 <mgovardhangoud311@gmail.com>
2024-06-24 06:29:46 +0530

    arguments concept in variables

---
+++ 02-01-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hi $PERSON2, Good Morning!"
+echo "$PERSON2:: Hi $PERSON1, Very Good morining"
+echo "$PERSON1:: How are you $PERSON2?"
+echo "$PERSON2:: Iam fine $PERSON1 how about you?"
+echo "$PERSON1:: Iam also good $PERSON2"
