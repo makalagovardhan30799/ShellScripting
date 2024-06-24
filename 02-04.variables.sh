c71ca513cb4d44d3b3ec90a27b520b80b0178bb7
makalagovardhan30799 <mgovardhangoud311@gmail.com>
2024-06-24 06:55:21 +0530

    concept in variables

---
+++ 02-04.variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Please enter username for staragile consultancy: "
+read -s USERNAME
+echo "Please enter password for staragile consultancy: "
+read -s PASSWORD
+
+echo "username is: $USERNAME and password is : $PASSWORD"
